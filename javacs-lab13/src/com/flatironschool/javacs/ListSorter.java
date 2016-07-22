443bf589af477670ca79e2022dccfcedb9fd5bc9
helenxiong12 <helenxiong12@gmail.com>
2016-07-22 01:25:35 -0700

    Done.

--- javacs-lab13/src/com/flatironschool/javacs/ListSorter.java
+++ javacs-lab13/src/com/flatironschool/javacs/ListSorter.java
@@ -53,6 +53,30 @@ public class ListSorter<T> {
 		list.addAll(sorted);
 	}
 
+	public List<T> merge(List<T> first, List<T> second, Comparator<T> comparator) {
+		List<T> sorted = new ArrayList<T>();
+		int total = first.size() + second.size();
+		int first_i = 0;
+		int second_i = 0;
+		while (first_i < first.size() && second_i < second.size()) {
+			if (comparator.compare(first.get(first_i), second.get(second_i)) < 0) {
+				sorted.add(first.get(first_i));
+				first_i++;
+			} else {
+				sorted.add(second.get(second_i));
+				second_i++;
+			}
+		}
+		while (first_i < first.size()) {
+			sorted.add(first.get(first_i));
+			first_i++;
+		}
+		while (second_i < second.size()) {
+			sorted.add(second.get(second_i));
+			second_i++;
+		}
+		return sorted;
+	}
 	/**
 	 * Sorts a list using a Comparator object.
 	 * 
@@ -63,8 +87,21 @@ public class ListSorter<T> {
 	 * @return
 	 */
 	public List<T> mergeSort(List<T> list, Comparator<T> comparator) {
-        // FILL THIS IN!
-        return null;
+        List<T> sortedList = new ArrayList<T>();
+        if (list.size() == 1) return list;
+
+        int half = list.size()/2;
+
+        //sort first half
+        List<T> firstHalf = list.subList(0, half);
+        insertionSort(firstHalf, comparator);
+        //sort second half
+        List<T> secondHalf = list.subList(half, list.size());
+        insertionSort(secondHalf, comparator);
+
+		sortedList = merge(firstHalf, secondHalf, comparator);
+       
+		return sortedList;
 	}
 
 	/**
@@ -74,8 +111,49 @@ public class ListSorter<T> {
 	 * @param comparator
 	 * @return
 	 */
+	public int max_index;
+
+	public void swap(List<T> list, int i, int j) {
+		T tmp = list.get(i);
+		list.set(i, list.get(j));
+		list.set(j, tmp);
+	}
+
+	public void maxheap(List<T> list, int i, Comparator<T> comparator) {
+		int max_index = list.size() - 1;
+		int left = 2*i;
+		int right = 2*i + 1;
+		int max = i;
+		if (left <= max_index && comparator.compare(list.get(i), list.get(left)) > 0) 
+		{
+			max = left;
+		}
+		if (right <= max_index && comparator.compare(list.get(max), list.get(right)) > 0)
+		{
+			max = right;
+		}
+
+		if (max != i) 
+		{
+			swap(list, i, max);
+			maxheap(list, max, comparator);
+		}
+	}
+
+	public void heapify(List<T> list, Comparator<T> comparator) {
+		max_index = list.size() - 1;
+		for (int i = max_index/2; i >= 0; i--) {
+			maxheap(list, i, comparator);
+		}
+	}
+
 	public void heapSort(List<T> list, Comparator<T> comparator) {
-        // FILL THIS IN!
+        heapify(list, comparator);
+        for (int i = max_index; i > 0; i--) {
+        	swap(list, 0, i);
+        	max_index = max_index - 1;
+        	maxheap(list, 0, comparator);
+        }
 	}
 
 	
@@ -89,8 +167,22 @@ public class ListSorter<T> {
 	 * @return
 	 */
 	public List<T> topK(int k, List<T> list, Comparator<T> comparator) {
-        // FILL THIS IN!
-        return null;
+        List<T> sorted = new ArrayList<T>();
+        while (list.isEmpty() == false) {
+        	T t = list.remove(0);
+        	if (sorted.size() > k) {
+        		if (comparator.compare(t, sorted.get(0)) > 0) {
+        			sorted.remove(0);
+        			sorted.add(t);
+        			heapSort(sorted, comparator);
+        		} //ignore if t is smaller than sorted
+        	} else {
+        		sorted.add(t);
+        		heapSort(sorted, comparator);
+        	}
+        }
+        sorted.remove(0);
+        return sorted;
 	}
 
 	
